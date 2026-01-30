46218181ef759696efdb80eb32f87f622c88f988
Timaa <timaa.at27@gmail.com>
2026-01-30 20:41:53 +0200

    Update categories_M.js

--- model/categories_M.js
+++ model/categories_M.js
@@ -11,7 +11,6 @@ async function add({name,userId}){
     let [result] = await db.query(sql,[name,userId]); 
     return result.insertId;
 }
-
 async function getOne(catId,userId){
     let sql = `SELECT * FROM categorias WHERE id = ? AND user_id = ?`;
     let [result] = await db.query(sql,[catId,userId]);    
