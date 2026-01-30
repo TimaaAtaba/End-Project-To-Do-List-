7fabe4ecac46811b0f9455b9040cca11c14d49cc
Timaa <timaa.at27@gmail.com>
2026-01-30 20:58:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookies());
 
-+
 app.use('/',require('./routes/pages_R'));
 app.use('/users',require('./routes/users_R'));
 app.use('/auth',require('./routes/auth_R'));
