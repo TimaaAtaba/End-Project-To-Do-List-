1870eebbfb54268c50b81beb28e4ac59d706705b
Timaa <timaa.at27@gmail.com>
2026-01-30 21:05:50 +0200

    Update tasks_MID.js

--- middleware/tasks_MID.js
+++ middleware/tasks_MID.js
@@ -4,6 +4,7 @@ function valuesToAdd(req,res,next){
       return res.status(400).json({message:"Missing data"});    }
     next();
 }
+
 function isValidId(req,res,next){
     let id = parseInt(req.params.id);
     if(isNaN(id) || id <= 0){
