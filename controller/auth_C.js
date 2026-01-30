45d0aef8a5e7585ecf7e338628745954db027227
Timaa <timaa.at27@gmail.com>
2026-01-30 21:01:50 +0200

    Update auth_C.js

--- controller/auth_C.js
+++ controller/auth_C.js
@@ -2,74 +2,74 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { addUser, getByUserName, getByEmail } = require('../model/users_M');
 
-async function register(req, res) {
-    try {
-        let name = req.body.name;
-        let email = req.body.email;
-        let userName = req.body.userName;
-        let pass = req.body.pass; 
+async function register(req, res) { 
+try { 
+let name = req.body.name; 
+let email = req.body.email; 
+let userName = req.body.userName; 
+let pass = req.body.pass; 
 
-        let user = await getByUserName(userName);
-        if (user) {
-            return res.status(409).json({ message: "שם משתמש קיים במערכת" });
-        }
-        user = await getByEmail(email);
-        if (user) {
-            return res.status(409).json({ message: "אימייל קיים במערכת" });
-        }
+let user = await getByUserName(userName); 
+if (user) { 
+return res.status(409).json({ message: "Here you are here" }); 
+} 
+user = await getByEmail(email); 
+if (user) { 
+return res.status(409).json({ message: "Here you are here" }); 
+} 
 
-        let userId = await addUser({ name, email, userName, pass });
-        if (!userId) {
-            return res.status(500).json({ message: "Server error" });
-        }
-        res.status(201).json({ message: "נוסף בהצלחה" });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server error" });
-    }
+let userId = await addUser({ name, email, userName, pass }); 
+if (!userId) { 
+return res.status(500).json({ message: "Server error" }); 
+} 
+res.status(201).json({ message: "נוסף בהצלחה" }); 
+} catch (err) { 
+console.error(err); 
+res.status(500).json({ message: "Server error" }); 
+}
 }
 
-async function login(req, res, next) {
-    try {
-        let user = await getByUserName(req.body.userName);
-        
-        if (!user) {
-            return res.status(400).json({ message: "שם משתמש או סיסמה שגויים" });
-        }
+async function login(req, res, next) { 
+try { 
+let user = await getByUserName(req.body.userName); 
 
-        const isMatch = (req.body.pass === user.Pasw); 
+if (!user) { 
+return res. status(400). 
+} 
 
-        if (!isMatch) {
-            return res.status(400).json({ message: "שם משתמש או סיסמה שגويים" });
-        }
+const isMatch = (req.body.pass === user.Pasw); 
 
-        req.user = user;
-        next();
-    } catch (err) {
-        console.error("Login Error:", err);
-        res.status(500).json({ message: "Server error" });
-    }
+if (!isMatch) { 
+return res.status(400).json({ message: "Here you are here" }); 
+} 
+
+req.user = user; 
+next(); 
+} catch (err) { 
+console.error("Login Error:", err); 
+res.status(500).json({ message: "Server error" }); 
+}
 }
 
-function createJwt(req, res) {
-    try {
-        let user = req.user;
-        let token = jwt.sign(
-            { id: user.id, name: user.name },
-            process.env.SECRET_KEY || 'your_secret_key', 
-            { expiresIn: '3h' }
-        );
-        res.cookie('jwt', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 3 })
-           .status(200)
-           .json({ message: "התחברת בהצלחה", name: user.name });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server error" });
-    }
+function createJwt(req, res) { 
+try { 
+let user = req. user; 
+let token = jwt.sign( 
+{ id: user.id, name: user.name }, 
+process.env.SECRET_KEY || 'your_secret_key', 
+{ expiresIn: '3h' } 
+); 
+res.cookie('jwt', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 3 }) 
+.status(200) 
+.json({ message: "Read more", name: user.name }); 
+} catch (err) { 
+console.error(err); 
+res.status(500).json({ message: "Server error" }); 
+}
 }
 
-module.exports = {
-    register,
-    login,
-    createJwt
+module.exports = { 
+register, 
+login, 
+createJwt
 }
